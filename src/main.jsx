ed12a8ab6d888d70299c0e10d4703e10988304e2
Big Burger <110642010+Alreeshid@users.noreply.github.com>
2024-08-16 14:34:55 -0400

    update 1 for mor einstalls

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import Homepage from './Components/Homepage'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Homepage />
   </StrictMode>,
 )
