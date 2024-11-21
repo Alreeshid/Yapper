261f330ee44ebf85b9b94a56dc3a44f5fdf717a8
Big Burger <110642010+Alreeshid@users.noreply.github.com>
2024-11-21 18:57:01 -0500

    display

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -34,7 +34,7 @@ const [data, setPageData] = useState([])
 */
   return (
     <>
-    <div className="bg-stone-400">
+    <div className="bg-stone-300">
       <br></br>
       <header>
         <div className="lg:flex">
@@ -65,7 +65,7 @@ const [data, setPageData] = useState([])
                 </a>
                 <a
                   className="block transform rounded-md bg-gray-200 px-5 py-2 text-center text-sm font-medium tracking-wider text-gray-700 transition-colors duration-300 hover:bg-gray-300 lg:mx-4"
-                  href="#"
+                  href="/#About"
                   target="_self">
                   Learn More About The Project
                 </a>
@@ -446,174 +446,10 @@ const [data, setPageData] = useState([])
                 </p>
               </div>
             </div>
-            <div>
-              <div className="bg-primary-600 inline-block rounded-lg p-3 text-white">
-                <span className="w-24px h-24px h-6 w-6">
-                  <svg
-                    className="w-[inherit] h-[inherit]"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                  </svg>
-                </span>
-              </div>
-              <div>
-                <h2 className="text-xl font-semibold text-gray-700 dark:text-white">
-                  
-                  Is the cost of the appoinment covered by private health insurance?
-                </h2>
-                <p className="mt-2 text-sm text-gray-500 dark:text-gray-300">
-                  
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident placeat, consequatur eveniet
-                  veritatis quos dignissimos beatae dolores exercitationem laboriosam officia magnam atque blanditiis
-                  illum doloremque magni ex corrupti tempora quis.
-                </p>
-              </div>
-            </div>
-            <div>
-              <div className="bg-primary-600 inline-block rounded-lg p-3 text-white">
-                <span className="w-24px h-24px h-6 w-6">
-                  <svg
-                    className="w-[inherit] h-[inherit]"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                  </svg>
-                </span>
-              </div>
-              <div>
-                <h2 className="text-xl font-semibold text-gray-700 dark:text-white">
-                  
-                  What is your cancellation policy?
-                </h2>
-                <p className="mt-2 text-sm text-gray-500 dark:text-gray-300">
-                  
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident placeat, consequatur eveniet
-                  veritatis quos dignissimos beatae dolores exercitationem laboriosam officia magnam atque blanditiis
-                  illum doloremque magni ex corrupti tempora quis.
-                </p>
-              </div>
-            </div>
-            <div>
-              <div className="bg-primary-600 inline-block rounded-lg p-3 text-white">
-                <span className="w-24px h-24px h-6 w-6">
-                  <svg
-                    className="w-[inherit] h-[inherit]"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                  </svg>
-                </span>
-              </div>
-              <div>
-                <h2 className="text-xl font-semibold text-gray-700 dark:text-white">
-                  
-                  What are the parking and public transport options?
-                </h2>
-                <p className="mt-2 text-sm text-gray-500 dark:text-gray-300">
-                  
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident placeat, consequatur eveniet
-                  veritatis quos dignissimos beatae dolores exercitationem laboriosam officia magnam atque blanditiis
-                  illum doloremque magni ex corrupti tempora quis.
-                </p>
-              </div>
-            </div>
           </div>
         </div>
       </section>
-      <section>
-        <div className="container mx-auto px-6 py-12">
-          <div className="text-center">
-            <p className="text-primary-500 dark:text-primary-400 font-medium"> Contact us </p>
-            <h2 className="mt-2 text-2xl font-semibold text-gray-800 md:text-3xl dark:text-white"> Get in touch </h2>
-            <p className="mt-3 text-gray-500 dark:text-gray-400">I'm always willing to answer questions and </p>
-          </div>
-          <div className="mt-10 grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-3">
-            <div className="flex flex-col items-center justify-center text-center">
-              <span className="bg-primary-100/80 text-primary-500 rounded-full p-3 dark:bg-gray-800">
-                <span className="w-24px h-24px h-6 w-6">
-                  <svg
-                    className="w-[inherit] h-[inherit]"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor">
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"></path>
-                  </svg>
-                </span>
-              </span>
-              <h2 className="mt-4 text-lg font-medium text-gray-800 dark:text-white"> Email </h2>
-              <p className="mt-2 text-gray-500 dark:text-gray-400"> Our friendly team is here to help. </p>
-              <p className="text-primary-500 dark:text-primary-400 mt-2"> hello@merakiui.com </p>
-            </div>
-            <div className="flex flex-col items-center justify-center text-center">
-              <span className="bg-primary-100/80 text-primary-500 rounded-full p-3 dark:bg-gray-800">
-                <span className="w-24px h-24px h-6 w-6">
-                  <svg
-                    className="w-[inherit] h-[inherit]"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"></path>
-                  </svg>
-                </span>
-              </span>
-              <h2 className="mt-4 text-lg font-medium text-gray-800 dark:text-white"> Office </h2>
-              <p className="mt-2 text-gray-500 dark:text-gray-400"> Come say hello at our office HQ. </p>
-              <p className="text-primary-500 dark:text-primary-400 mt-2"> 100 Smith Street Collingwood VIC 3066 AU </p>
-            </div>
-            <div className="flex flex-col items-center justify-center text-center">
-              <span className="bg-primary-100/80 text-primary-500 rounded-full p-3 dark:bg-gray-800">
-                <span className="w-24px h-24px h-6 w-6">
-                  <svg
-                    className="w-[inherit] h-[inherit]"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor">
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"></path>
-                  </svg>
-                </span>
-              </span>
-              <h2 className="mt-4 text-lg font-medium text-gray-800 dark:text-white"> Phone </h2>
-              <p className="mt-2 text-gray-500 dark:text-gray-400"> Mon-Fri from 8am to 5pm. </p>
-              <p className="text-primary-500 dark:text-primary-400 mt-2"> +1 (555) 000-0000 </p>
-            </div>
-          </div>
-        </div>
-        </section>
+    
         <br></br><hr></hr>
         <div className="container mx-auto px-6 py-8">
       <hr className="my-10 border-gray-200 dark:border-gray-700" />
