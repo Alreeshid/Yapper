5fa074789c92c890a73118fda65ed24e694af92c
Big Burger <110642010+Alreeshid@users.noreply.github.com>
2024-11-21 21:48:38 -0500

    this or fail

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,8 @@
-import app from "../backend/server";
+const express = require("express");
+const app = express();
 
-export app;
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
